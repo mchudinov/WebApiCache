ac08a58a4306a158279d0c67a63a30ff6c6f1b57
Mikael Chudinov <mikael@chudinov.net>
2017-02-20 15:25:40 +0100

    Add WebApi client project.

---
+++ WebApiClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace WebApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string uri = "http://localhost:8080/api/Cache";
+
+            using (var wc = new WebClient())
+            {
+                Console.WriteLine("Run GET request...");
+                var temp = wc.DownloadString(uri);
+                Console.WriteLine(temp);
+
+                Console.WriteLine("Run POST request. Reset cache");
+                wc.UploadString(uri,string.Empty);
+
+                Console.WriteLine("Run GET request...");
+                temp = wc.DownloadString(uri);
+                Console.WriteLine(temp);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
