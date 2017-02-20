bfdfa2d2fd93d11289f9521cc59b3b32e355168b
Mikael Chudinov <mikael@chudinov.net>
2017-02-20 15:51:06 +0100

    Use interactive input in WebApi client.

--- WebApiClient/Program.cs
+++ WebApiClient/Program.cs
@@ -5,27 +5,46 @@ namespace WebApiClient
 {
     class Program
     {
-        static void Main(string[] args)
-        {            
-            string uri = "http://localhost:8080/api/Cache";
+        private const string Uri = "http://localhost:8080/api/Cache";
 
+        static void Main(string[] args)
+        {
             using (var wc = new WebClient())
             {
-                Console.WriteLine("Run GET request...");
-                var temp = wc.DownloadString(uri);
-                Console.WriteLine(temp);
-
-                Console.WriteLine("Run POST request. Reset cache");
-                wc.UploadString(uri,string.Empty);
-
-                Console.WriteLine("Run GET request...");
-                temp = wc.DownloadString(uri);
-                Console.WriteLine(temp);
+                GetRequest();
+                PostRequest();
             }
 
+            Console.WriteLine("Type g - for Get request, p - for POst request, e - for exit.");
 
+            var input = string.Empty;
+            while (input != "e")
+            {
+                input = Console.ReadLine();
+                if (input == "g")
+                    GetRequest();
+                if(input == "p")
+                    PostRequest();
+            }
+        }
 
-            Console.ReadLine();
+        private static void GetRequest()
+        {
+            using (var wc = new WebClient())
+            {
+                Console.Write("Run GET request... ");
+                var response = wc.DownloadString(Uri);
+                Console.WriteLine("Response:" + response);
+            }
+        }
+
+        private static void PostRequest()
+        {
+            using (var wc = new WebClient())
+            {
+                Console.WriteLine("Run POST request. Reset cache");
+                wc.UploadString(Uri, string.Empty);
+            }
         }
     }
 }
